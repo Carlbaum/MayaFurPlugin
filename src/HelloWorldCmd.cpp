363801b823b3e14f2d3828c046e5e3925e68c8b2
Daniel Holst <danielholst2@gmail.com>
2016-11-28 13:52:55 +0100

    simple draw line

--- src/HelloWorldCmd.cpp
+++ src/HelloWorldCmd.cpp
@@ -1,19 +1,37 @@
-//-
-// ==========================================================================
-// Copyright 1995,2006,2008 Autodesk, Inc. All rights reserved.
-//
-// Use of this software is subject to the terms of the Autodesk
-// license agreement provided at the time of installation or download,
-// or which otherwise accompanies this software in either electronic
-// or hard copy form.
-// ==========================================================================
-//+
 
-#include <iostream>
-#include <maya/MSimple.h>
+#include <math.h>
 #include <maya/MIOStream.h>
-DeclareSimpleCommand(HelloWorld, "Autodesk", "2017");
-MStatus HelloWorld::doIt(const MArgList&) {
-  std::cout << "Hello World\n" << std::endl;
-  return MS::kSuccess;
+#include <maya/MSimple.h>
+#include <maya/MPoint.h>
+#include <maya/MPointArray.h>
+#include <maya/MDoubleArray.h>
+#include <maya/MFnNurbsCurve.h>
+DeclareSimpleCommand( doHelix, "Autodesk - Example", "2017");
+MStatus doHelix::doIt( const MArgList& )
+{
+    MStatus stat;
+    const unsigned deg = 1; // Curve Degree
+    const unsigned ncvs = 10; // Number of CVs
+    const unsigned spans = ncvs - deg; // Number of spans
+    const unsigned nknots = spans+2*deg-1; // Number of knots
+    double radius = 1.0; // Helix radius
+    double pitch = 0.5; // Helix pitch
+    unsigned i;
+    MPointArray controlVertices;
+    MDoubleArray knotSequences;
+    // Set up cvs and knots for the helix
+    //
+    for (i = 0; i < ncvs; i++)
+        controlVertices.append( MPoint( radius * cos( (double)i ),
+        pitch * (double)i, radius * sin( (double)i ) ) );
+    for (i = 0; i < nknots; i++)
+        knotSequences.append( (double)i );
+    // Now create the curve
+    //
+    MFnNurbsCurve curveFn;
+    MObject curve = curveFn.create( controlVertices,
+        knotSequences, deg, MFnNurbsCurve::kOpen, false, false, MObject::kNullObj, &stat );
+    if ( MS::kSuccess != stat )
+        cout << "Error creating curve.\n";
+    return stat;
 }

b424b8e99a7122deee6850ba476599f08024f5ab
novalain <michael.f.noven@gmail.com>
2016-11-29 15:38:15 +0100

    Populate vector with 1 strand when init plugin

--- src/HelloWorldCmd.cpp
+++ src/HelloWorldCmd.cpp
@@ -10,73 +10,72 @@
 #include <maya/MFnUnitAttribute.h>
 
 // Macro for debugging
-#define McheckErr(stat,msg)         \
-    if ( MS::kSuccess != stat ) {   \
-        cerr << msg;                \
-        return MS::kFailure;        \
-    }
+#define McheckErr(stat, msg)  \
+  if (MS::kSuccess != stat) { \
+    cerr << msg;              \
+    return MS::kFailure;      \
+  }
 
 // TODO: Move to separate class with header files and stuff
 class FurrySystem : public MPxNode {
-  public:
-    static MTypeId id; // Some hash to identify plugin with
-    static MObject time;
+ public:
+  static MTypeId id;  // Some hash to identify plugin with
+  static MObject time;
+  static std::vector<Strand> strands;
 
-    FurrySystem() {};
-    virtual ~FurrySystem() {};
-    static void* creator() {
-      return new FurrySystem;
-    };
+  FurrySystem(){};
+  virtual ~FurrySystem(){};
 
-    // From old doIt
-    static MStatus initialize() {
-      MStatus stat;
-      const unsigned num_segments = 10;
-      float strand_length = 10.f;
-      cout << "hej hoppppppppp.\n";
+  static void* creator() { return new FurrySystem; };
+  // From old doIt
+  static MStatus initialize() {
+    cout << ">> Initializing";
+    MStatus stat;
 
-      MFnUnitAttribute unitAttr;
-      time = unitAttr.create( "time", "tm",
-                                          MFnUnitAttribute::kTime,
-                                          0.0, &stat);
-      McheckErr(stat, "ERROR creating time attribute\n");
-      stat = addAttribute(time);
-      McheckErr(stat, "ERROR adding time attribute\n");
-
-      Strand* strand_0 = new Strand(
-          MPoint( 0.0, 0.0, 0.0 ),
-          num_segments,
-          MVector( 0,-1,0 ),
-          15.f );
-      cout << "KALAS!!.\n";
-      MFnNurbsCurve curveFn;
-      MObject curve = curveFn.create( strand_0->point_positions,
-          strand_0->knot_sequences, strand_0->degree, MFnNurbsCurve::kOpen, false, false, MObject::kNullObj, &stat );
-      cout << "FANTASTISKT.\n";
-      if ( MS::kSuccess != stat )
-          cout << "Error creating curve.\n";
-      //delete strand_0; // DO THIS IN DEINITIALIZE PLUGIN
-      return stat;
-      //return MS::kSuccess;
+    MFnUnitAttribute unitAttr;
+    time = unitAttr.create("time", "tm", MFnUnitAttribute::kTime, 0.0, &stat);
+    McheckErr(stat, "ERROR creating time attribute\n");
+    stat = addAttribute(time);
+    McheckErr(stat, "ERROR adding time attribute\n");
+    if (MS::kSuccess != stat) {
+      cout << "Error creating curve.\n";
     }
+    stat = createStrands(stat);
+    return stat;
+  }
 
-    virtual MStatus compute(const MPlug& plug, MDataBlock& data) {
-      std::cout << "Computing .." << std::endl;
-      return MS::kSuccess;
-    }
+  static MStatus createStrands(MStatus& stat) {
+    // delete strand_0; // DO THIS IN DEINITIALIZE PLUGIN
+    const unsigned num_segments = 10;
+    float strand_length = 10.f;
+    Strand strand_0 =
+        Strand(MPoint(0.0, 0.0, 0.0), num_segments, MVector(0, -1, 0), 15.f);
+    strands.push_back(strand_0);
+    MFnNurbsCurve curveFn;
+    MObject curve = curveFn.create(
+        strand_0.point_positions, strand_0.knot_sequences, strand_0.degree,
+        MFnNurbsCurve::kOpen, false, false, MObject::kNullObj, &stat);
+    return stat;
+  }
+
+  virtual MStatus compute(const MPlug& plug, MDataBlock& data) {
+    cout << ">> Computing ..\n";
+    return MS::kSuccess;
+  }
 };
 
 // Initialize static variables
 MObject FurrySystem::time;
 MTypeId FurrySystem::id( 0x80000 );
-
+std::vector<Strand> FurrySystem::strands;
 
 // Globals
 MStatus initializePlugin(MObject obj) {
-  std::cout << ">> Initializing plugin .. " << std::endl;
+  cout << ">> Initializing plugin .. \n";
   MStatus   status;
   MFnPlugin plugin(obj, "Kebaum, Nova, Kingen", "0.1", "Any");
-  status = plugin.registerNode("FurrySystem", FurrySystem::id, FurrySystem::creator, FurrySystem::initialize);
+  status = plugin.registerNode("FurrySystem", FurrySystem::id,
+                               FurrySystem::creator, FurrySystem::initialize);
   if (!status) {
     status.perror("registerNode");
     return status;
@@ -85,7 +84,7 @@ MStatus initializePlugin(MObject obj) {
 }
 
 MStatus uninitializePlugin(MObject obj) {
-  std::cout << ">> Uninitializing plugin " << std::endl;
+  cout << ">> Uninitializing plugin \n";
   MStatus   status;
   MFnPlugin plugin(obj);
   status = plugin.deregisterNode(FurrySystem::id);
